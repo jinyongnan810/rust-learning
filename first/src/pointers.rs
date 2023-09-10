47712cef54e013f9db0cd3ef8f948d4721a12b66
Kin Yuunan <yuunan.kin@gmail.com>
2023-09-11 08:35:00 +0900

    pointers

--- first/src/pointers.rs
+++ first/src/pointers.rs
@@ -1,6 +1,8 @@
 #![deny(clippy::all)]
 
+use std::cell::Cell;
 use std::ops::Deref;
+use std::rc::Rc;
 
 struct BoxedValue<T> {
     value: T,
@@ -18,6 +20,14 @@ impl<T> Deref for BoxedValue<T> {
     }
 }
 
+fn print_number(number: &i32) {
+    println!("number is {}", number);
+}
+struct Person {
+    name: String,
+    age: Cell<u8>,
+}
+
 pub fn pointers() {
     println!("----------pointers----------");
     let age = Box::new(37);
@@ -30,4 +40,23 @@ pub fn pointers() {
     println!("double age is {}", self_made_box.deref() * 2);
     let ref_of_self_made_box = self_made_box.deref();
     println!("Age is {}", *ref_of_self_made_box);
+    // passing a reference of box to a function
+    print_number(&self_made_box);
+
+    // reference counting
+    let array = [1, 2, 3];
+    let rc = Rc::new(array);
+    println!("array = {:?}", array);
+    println!("rc = {:?}", rc);
+    drop(rc);
+    // println!("rc = {:?}", rc)
+    println!("array = {:?}", array);
+
+    // Cell allows mutation inside immutable references
+    let person = Person {
+        name: String::from("Alice"),
+        age: Cell::new(37),
+    };
+    person.age.set(38);
+    println!("Person({})'s age is now {}", person.name, person.age.get());
 }
